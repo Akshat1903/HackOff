a77aa9738f6e148f81f59c9e0b537ac22064b422
Shubh Gupta <shubhngupta04@gmail.com>
2020-12-12 18:03:49 +0530

    Chain created

--- filelocker/blockchain/admin.py
+++ filelocker/blockchain/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import BlockChain
+from django.contrib import messages
 
 # Register your models here.
 @admin.register(BlockChain)
--- filelocker/blockchain/models.py
+++ filelocker/blockchain/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from hashlib import sha256
-
+from django.contrib import messages
 # Create your models here.
 
 class BlockChain(models.Model):
@@ -15,17 +15,24 @@ class BlockChain(models.Model):
 
     def save(self, *args, **kwargs):
         genesis_data = 'genesis data'
+        print(args,kwargs)
         blocks = BlockChain.objects.all().count()
 
         if(blocks == 0):
             self.previous_hash = sha256(
             u'{}'.format(genesis_data).encode('utf-8')).hexdigest()
+            super(BlockChain, self).save(*args, **kwargs)
 
         else:
             if(self.id == None):
                 last_block_hash = BlockChain.objects.get(id = blocks).hash
                 self.previous_hash = last_block_hash
-        super(BlockChain, self).save(*args, **kwargs)
+                super(BlockChain, self).save(*args, **kwargs)
+            for i in range(2,BlockChain.objects.all().count() + 1):
+                current_block_previous_hash = BlockChain.objects.get(id = i).previous_hash
+                previous_block_hash = BlockChain.objects.get(id = i-1).hash
+                if current_block_previous_hash != previous_block_hash:
+                    messages.error(self.request,'Someone Changed the block')
 
     @property
     def hash(self):
